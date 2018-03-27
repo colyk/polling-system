6965247f21c9deb57bc35a7d18349034cfbd1c16
colyk <colyk@yandex.ru>
2018-03-27 19:47:49 +0200

    Testing flask-rest

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 == Installing ==
 
 * pip install virtualenv
+* pip install flask-restful
 * virtualenv env
 * env\Scripts\activate
 * pip install flask
--- server.py
+++ server.py
@@ -1,9 +1,19 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Resource, Api
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'body': 'Hello world!'}
+
+class Lol(Resource):
+    def get(self):
+        return {'body': 'Lol'}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Lol, '/lol')
 
 if __name__ == '__main__':
     app.run(debug=True)
