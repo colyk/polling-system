b4b39455963510cfc066cd0872ae9bbef2a3f352
colyk <colyk@yandex.ru>
2018-03-27 20:26:30 +0200

    Testing flask-rest

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # polling-system
 
-== Installing ==
+## How to run project
 
-* pip install virtualenv
-* pip install flask-restful
-* virtualenv env
-* env\Scripts\activate
-* pip install flask
+
+```
+pip install virtualenv
+virtualenv env
+env\Scripts\activate
+
+pip install flask
+pip install flask-restful
+
+python server.py
+```
+
+Now head over to http://127.0.0.1:5000/
 
 
--- server.py
+++ server.py
@@ -4,16 +4,25 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'body': 'Hello world!'}
 
-class Lol(Resource):
-    def get(self):
-        return {'body': 'Lol'}
+voteFor = ['Trump', 'Bush', 'Merkel']
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(Lol, '/lol')
+
+class MainPage(Resource):
+    def get(self):
+        return {'Title': 'Candidates for voting!',
+                'Candidates': voteFor,
+                }
+
+class Main(Resource):
+    def put(self):
+        print(request.form.keys)
+        return {"title": ";f"}
+
+
+api.add_resource(MainPage, '/')
+api.add_resource(Main, '/t')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
